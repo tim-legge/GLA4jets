01716ac927095fc2d7fcc62eeab2e6624c568906
tim_legge <tlegge@ucsd.edu>
2025-09-13 22:45:08 -0700

    do the inference

--- testing_model_noclass.py
+++ testing_model_noclass.py
@@ -1429,7 +1429,7 @@ def get_model(model_type='qg',**kwargs):
             activation='gelu',
             # misc
             trim=True,
-            for_inference=False,
+            for_inference=True,
         )
     
     cfg.update(**kwargs)
